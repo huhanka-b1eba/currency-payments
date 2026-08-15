6b62f42154a3617103b618a0a496eae91df8f45a
Aigiz Tulubaev <flaylay256@gmail.com>
2026-08-15 20:15:50 +0300

    feat: add form group

--- src/pages/create-payment/create-payment.component.ts
+++ src/pages/create-payment/create-payment.component.ts
@@ -1,9 +1,46 @@
-import { Component } from '@angular/core';
+import {Component, signal} from '@angular/core';
+import {FormControl, FormGroup, ReactiveFormsModule, Validators} from '@angular/forms';
+import {switchMap, tap} from 'rxjs';
 
 @Component({
   selector: 'app-create-payment',
-  imports: [],
+  imports: [
+    ReactiveFormsModule
+  ],
   templateUrl: './create-payment.component.html',
   styleUrl: './create-payment.component.scss',
 })
-export class CreatePaymentComponent {}
+export class CreatePaymentComponent {
+
+  form = new FormGroup({
+    recipient: new FormControl('', {
+      validators: [
+        Validators.required,
+        Validators.minLength(2),
+      ],
+    }),
+
+    amount: new FormControl(0, {
+      nonNullable: true,
+      validators: [
+        Validators.required,
+        Validators.min(1),
+      ]
+    }),
+
+    currency: new FormControl<'USD' | 'EUR' | 'GBP'>('USD', {
+      nonNullable: true,
+      validators: [Validators.required]
+    })
+  })
+
+  submit() {
+    if (this.form.invalid) {
+      this.form.markAllAsTouched();
+      return;
+    }
+
+    console.log(this.form.getRawValue());
+  }
+
+}
--- src/shared/auth/auth.service.ts
+++ src/shared/auth/auth.service.ts
@@ -4,7 +4,7 @@ import { Injectable, signal } from '@angular/core';
   providedIn: 'root',
 })
 export class AuthService {
-  private readonly _isAuthenticated = signal(false);
+  private readonly _isAuthenticated = signal(true);
 
   readonly isAuthenticated =
     this._isAuthenticated.asReadonly();
